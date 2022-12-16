27747964356cf1518d1a85816da96d5a43e123fb
Mike Wu <hello@mikewu.dev>
2022-12-17 07:28:03 +0900

    Mission - grow background

--- src/Background.js
+++ src/Background.js
@@ -1,13 +1,43 @@
 import styled from 'styled-components'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import * as THREE from 'three'
-import { useCallback, useEffect, useRef } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { publicUrl } from './lib/url'
+import gsap from 'gsap'
+
+/**
+ * How much scroll is required before we expand the globe
+ * to be full screen.
+ */
+const EXPAND_SCROLL_THRESHOLD = 150
 
 export default function Background() {
   const containerRef = useRef()
   const rendererRef = useRef()
   const cameraRef = useRef()
+  const objRef = useRef()
+  const rotationSpeedRef = useRef(1)
+
+  const [expanded, setExpanded] = useState(false)
+
+  const scrollPosition = useScrollPosition()
+
+  useEffect(() => {
+    if (!objRef.current) {
+      return
+    }
+
+    const shouldExpand = scrollPosition > EXPAND_SCROLL_THRESHOLD
+    if (shouldExpand && !expanded) {
+      setExpanded(true)
+      return
+    }
+
+    if (!shouldExpand && expanded) {
+      setExpanded(false)
+      return
+    }
+  }, [scrollPosition, expanded])
 
   useEffect(() => {
     const { current: container } = containerRef
@@ -25,7 +55,6 @@ export default function Background() {
     container.appendChild(renderer.domElement)
 
     const scene = new THREE.Scene()
-    let obj = null
 
     const camera = new THREE.PerspectiveCamera(
       10,
@@ -33,7 +62,7 @@ export default function Background() {
       0.1,
       5000
     )
-    camera.position.set(0, 0, 300)
+    camera.position.set(0, 0, 500)
     cameraRef.current = camera
 
     camera.lookAt(-25, 0, 0)
@@ -43,9 +72,8 @@ export default function Background() {
       window.requestAnimationFrame(animate)
 
       // Spin planet
-      if (obj) {
-        const time = Date.now() * 0.0001
-        obj.rotation.y = -0.4 * time
+      if (objRef.current) {
+        objRef.current.rotation.y -= rotationSpeedRef.current * 0.001
       }
 
       renderer.render(scene, camera)
@@ -55,9 +83,9 @@ export default function Background() {
 
     // Load our planet 3d model
     loader.load(publicUrl('/planet-1.gltf'), (gltf) => {
-      obj = gltf.scene
+      objRef.current = gltf.scene
 
-      obj.traverse(function (child) {
+      objRef.current.traverse(function (child) {
         if (!child.isMesh) {
           return
         }
@@ -72,7 +100,7 @@ export default function Background() {
         child.scale.set(scale, scale, scale)
       })
 
-      scene.add(obj)
+      scene.add(objRef.current)
 
       // Kick off render, and animation...
       animate()
@@ -81,12 +109,10 @@ export default function Background() {
     // Add light to see object...
     const lightOne = new THREE.DirectionalLight(0xcccccc)
     lightOne.position.set(-1.5, -1, 1)
-    lightOne.intensity = 1.5
     scene.add(lightOne)
 
     const lightTwo = new THREE.DirectionalLight(0xffffff)
     lightTwo.position.set(0, 0, 1)
-    lightTwo.intensity = 0.9
     scene.add(lightTwo)
 
     return () => {
@@ -109,6 +135,7 @@ export default function Background() {
     }
   }, [])
 
+  // Bind resize event to re-draw canvas
   useEffect(() => {
     window.addEventListener('resize', resizeCanvas)
     return () => {
@@ -116,12 +143,54 @@ export default function Background() {
     }
   }, [resizeCanvas])
 
+  // Handle expanded state to scale planet
+  useEffect(() => {
+    if (!objRef.current) {
+      return
+    }
+
+    const scale = expanded ? 10 : 1
+
+    gsap.to(objRef.current.scale, {
+      duration: 1.8,
+      x: scale,
+      y: scale,
+      z: scale,
+      ease: 'power1.inOut',
+    })
+
+    // Adjust rotation speed according to zoom
+    gsap.to(rotationSpeedRef, {
+      duration: 1.8,
+      current: expanded ? 0.1 : 1,
+      ease: 'none', // Linear
+    })
+  }, [expanded])
+
   return <Container ref={containerRef} />
+}
+
+function useScrollPosition() {
+  const [position, setPosition] = useState(0)
+  const handleScroll = () => {
+    const position = window.pageYOffset
+    setPosition(position)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true })
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return position
 }
 
 const Container = styled.div`
   z-index: -1;
-  position: absolute;
+  position: fixed;
   height: 100%;
   width: 100%;
   top: 0;
