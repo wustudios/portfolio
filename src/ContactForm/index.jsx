55eb679f955e61fcf7ec8b76ade948113d75a41b
Mike Wu <hello@mikewu.dev>
2022-12-19 01:00:58 +0900

    Mission - add links

--- src/ContactForm/index.jsx
+++ src/ContactForm/index.jsx
@@ -1,11 +1,13 @@
 import Logo from '../lib/Logo'
 import TextField from '@mui/material/TextField'
+import { Link } from 'react-router-dom'
 
 export default function ContactForm() {
   return (
     <>
-      <Logo compact />
-
+      <Link to="/">
+        <Logo compact />
+      </Link>
       <h2>Get in touch</h2>
       <TextField variant="outlined" placeholder="Name" />
       <TextField variant="outlined" multiline rows={5} />
