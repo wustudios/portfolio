6c760c515e24824c82f5f29f710022cc2279cce2
Mike Wu <hello@mikewu.dev>
2023-01-24 22:08:22 +0900

    Contact- submit form

--- src/Home/ContactForm.jsx
+++ src/Home/ContactForm.jsx
@@ -2,8 +2,26 @@ import TextField from '@mui/material/TextField'
 import Box from '@mui/material/Box'
 import Button from '../lib/Button'
 import Section from './Section'
+import { useForm } from 'react-hook-form'
+
+const FORM_NAME = 'contact'
 
 export default function ContactForm() {
+  const { register, handleSubmit } = useForm()
+
+  const submit = (data) => {
+    console.log(data)
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: urlEncode({
+        'form-name': FORM_NAME,
+        ...data,
+      }),
+    }).then(() => {
+      console.log('submitted!')
+    })
+  }
   return (
     <Section id="contact">
       <div>
@@ -11,25 +29,53 @@ export default function ContactForm() {
         <p>
           Have an idea in mind? Send us a message. We'd love to hear from you.
         </p>
-        <Box mb={1}>
-          <TextField variant="outlined" placeholder="Your Name" fullWidth />
-        </Box>
-        <Box mb={1}>
-          <TextField variant="outlined" placeholder="Your Email" fullWidth />
-        </Box>
-        <Box mb={1}>
-          <TextField
-            variant="outlined"
-            multiline
-            fullWidth
-            rows={5}
-            placeholder="Your Message"
-          />
-        </Box>
-        <Button size="large" fullWidth>
-          Send
-        </Button>
+        <form onSubmit={handleSubmit(submit)}>
+          <Box mb={1}>
+            <TextField
+              variant="outlined"
+              placeholder="Your Name"
+              fullWidth
+              name="name"
+              inputProps={{
+                ref: register,
+              }}
+            />
+          </Box>
+          <Box mb={1}>
+            <TextField
+              variant="outlined"
+              placeholder="Your Email"
+              fullWidth
+              name="email"
+              inputProps={{
+                ref: register,
+              }}
+            />
+          </Box>
+          <Box mb={1}>
+            <TextField
+              variant="outlined"
+              multiline
+              fullWidth
+              rows={5}
+              placeholder="Your Message"
+              name="message"
+              inputProps={{
+                ref: register,
+              }}
+            />
+          </Box>
+          <Button size="large" fullWidth type="submit">
+            Send
+          </Button>
+        </form>
       </div>
     </Section>
   )
+}
+
+function urlEncode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
 }
